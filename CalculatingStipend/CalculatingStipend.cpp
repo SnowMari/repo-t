4220ec78b4e523af3f0184fba08f0439c37066b9
Liza <avdeenko.l26@mail.ru>
2021-07-14 16:44:14 +0300

    Добавлена пустая голова списка и case вывода всех студентов

--- CalculatingStipend/CalculatingStipend.cpp
+++ CalculatingStipend/CalculatingStipend.cpp
@@ -16,7 +16,7 @@ struct StudentBaseDate
 	char group[SIZECHAR];
 	char surname[SIZECHAR];
     char name[SIZECHAR];
-	char Credit�ard[SIZECHAR];
+	char CreditCard[SIZECHAR];
 	double stipend;
 	double SumStipend;
 	double MTotalCash;
@@ -27,9 +27,8 @@ struct StudentBaseDate
 
 void _tmain()
 {
-    //SetConsoleCP(1251);
-    //SetConsoleOutputCP(1251);
-	setlocale(LC_ALL, "rus");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
 
 	int Menu();
 	int CmdMenu;
@@ -50,7 +49,65 @@ void _tmain()
 	<< std::endl;
 	std::cout<< "|===========================================|";
 
+	StudentBaseDate* head = new StudentBaseDate; 
+    head->next = NULL;
+    head->id = 0;
 
+
+	do
+	{
+		CmdMenu = Menu();
+		switch (CmdMenu)
+		{
+			case EXIT:
+				return;
+				break;
+			case PRINT_ALL_STUDENT:
+			{
+				StudentBaseDate* buffer = head->next;  
+				while (buffer->next != NULL)
+				{
+					std::cout << std::endl;
+					std::cout << "id - " << buffer->id 
+					<< std::endl;
+					std::cout << "Группа: " << buffer->group 
+					<< std::endl;
+					std::cout << "Фамилия и имя студента: "
+					<< buffer->surname 
+					<< ' '<<buffer->name << std::endl;
+					std::cout << "Номер зачётной книжки: " 
+					<< buffer->CreditCard << std::endl;
+					std::cout << "Стипендия в месяц: "
+					<< buffer->stipend<<std::endl;
+					std::cout << "Стипендия за семестр: "
+					<< buffer->SumStipend<<std::endl;
+
+					buffer = buffer->next; 
+					// меняем указатель для перебора элементов
+				}
+				std::cout << std::endl;
+				std::cout << "id - " << buffer->id 
+				<< std::endl;
+				std::cout << "Группа: " << buffer->group 
+				<< std::endl;
+				std::cout << "Фамилия и имя студента: "
+				<< buffer->surname 
+				<< ' '<<buffer->name 
+				<< std::endl;
+				std::cout << "Номер зачётной книжки: " 
+				<< buffer->CreditCard 
+				<< std::endl;
+				std::cout << "Стипендия в месяц: "
+				<< buffer->stipend
+				<<std::endl;
+				std::cout << "Стипендия за семестр: "
+				<< buffer->SumStipend
+				<<std::endl;
+			}
+			break;
+
+		}
+	}while(true);
 	system("pause");
 }
 
